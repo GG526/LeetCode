e1e3f2648d36124f42856353e12e08e46b1c5466
岚 <yanglanqing@qutoutiao.net>
2020-01-13 20:33:13 +0800

    添加TreeNode

--- LeetCode.playground/Sources/IsSymmetric.swift
+++ LeetCode.playground/Sources/IsSymmetric.swift
@@ -22,18 +22,6 @@ import Foundation
  */
 
 
-public class TreeNode {
-    public var val: Int
-    public var left: TreeNode?
-    public var right: TreeNode?
-    public init(_ val: Int) {
-        self.val = val
-        self.left = nil
-        self.right = nil
-    }
-}
- 
- 
 public func isSymmetric(_ root: TreeNode?) -> Bool {
     return equal(root, t2: root)
 }
--- LeetCode.playground/Sources/MaxDepth.swift
+++ LeetCode.playground/Sources/MaxDepth.swift
@@ -1,20 +1,6 @@
 import Foundation
 
 
-  
-//public class TreeNode {
-//    public var val: Int
-//    public var left: TreeNode?
-//    public var right: TreeNode?
-//    public init(_ val: Int) {
-//        self.val = val
-//        self.left = nil
-//        self.right = nil
-//    }
-//}
- 
-
-
 public func maxDepth(_ root: TreeNode?) -> Int {
     
     guard let root = root else {
