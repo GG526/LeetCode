45fbd4d2a5ff4b35c7775ed5e097ec81b7c67e29
山風 <lanqing98@126.com>
2020-01-14 20:25:55 +0800

    add maxProfit2

--- LeetCode.playground/Contents.swift
+++ LeetCode.playground/Contents.swift
@@ -1,3 +1,4 @@
 import Foundation
 
 
+maxProfit([7,6, 5, 4, 3, 2, 1])
--- LeetCode.playground/Sources/Generate.swift
+++ LeetCode.playground/Sources/Generate.swift
@@ -18,10 +18,25 @@ import Foundation
 
 
 public func generate(_ numRows: Int) -> [[Int]] {
-    
-    
-    
-    
-    
+    guard numRows > 0 else {
+        return []
+    }
+    var generate: [[Int]] = []
+    generate.append([1])
+    var i = 1
+    while i < numRows {
+        var list = [1]
+        let lastList = generate[i - 1]
+        var j = 0
+        while j < lastList.count - 1 {
+            let value = lastList[j] + lastList[j + 1]
+            list.append(value)
+            j += 1
+        }
+        list.append(1)
+        generate.append(list)
+        i += 1
+    }
+    return generate
 }
 
