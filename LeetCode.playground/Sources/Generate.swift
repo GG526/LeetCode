dbcc02c9b7cd02b140df37d221743ff55de29b8c
山风 <lanqing98@126.com>
2020-01-14 10:34:27 +0800

    ADD Generate

---
+++ LeetCode.playground/Sources/Generate.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+/*
+ 
+ 示例:
+
+ 输入: 5
+ 输出:
+ [
+      [1],
+     [1,1],
+    [1,2,1],
+   [1,3,3,1],
+  [1,4,6,4,1]
+ ]
+ 
+ */
+
+
+public func generate(_ numRows: Int) -> [[Int]] {
+    
+    
+    
+    
+    
+}
+
